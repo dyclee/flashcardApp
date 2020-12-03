6df223d7bed6a0f4b846336e2ba268675616f5a6
David <dyclee@umich.edu>
2020-12-03 17:32:33 -0600

    user in redux store

--- react-app/src/store/configureStore.js
+++ react-app/src/store/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import setReducer from './reducers/sets';
+import userReducer from './reducers/users';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
+    userReducer,
     setReducer,
 })
 
--- react-app/src/store/reducers/users.js
+++ react-app/src/store/reducers/users.js
@@ -1,1 +1,14 @@
 import { GET_USER, REMOVE_USER } from '../actions/users';
+
+export default function userReducer(state = {}, action) {
+    const newState = {...state }
+    switch(action.type) {
+        case GET_USER:
+            return action.user
+        case REMOVE_USER:
+            return {}
+        default:
+            return state
+
+    }
+}
