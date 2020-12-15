5b8287b869ff543a0d03e5e602a5826e4d2d4cfb
David <dyclee@umich.edu>
2020-12-15 17:14:30 -0600

    deploying

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,17 @@ db.init_app(app)
 Migrate(app, db)
 
 # Application Security
+
+
 CORS(app)
+
+
+@app.before_request
+def redirect_https():
+  if request.headers.get('X-Forwarded-Proto') == 'http':
+    url = request.url.replace('http://', 'https://', 1)
+    code = 301
+    return redirect(url, code=code)
 
 
 @app.after_request
