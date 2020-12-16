1aaad261872daa1962da14ea8839ef3709dad45b
David <dyclee@umich.edu>
2020-12-15 18:58:21 -0600

    refactored

--- app/__init__.py
+++ app/__init__.py
@@ -50,12 +50,13 @@ Migrate(app, db)
 CORS(app)
 
 
-# @app.before_request
-# def redirect_https():
-#     if request.headers.get('X-Forwarded-Proto') == 'http':
-#         url = request.url.replace('http://', 'https://', 1)
-#         code = 301
-#         return redirect(url, code=code)
+@app.before_request
+def redirect_https():
+    if os.environ.get("FLASK_ENV") == "production":
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
 
 
 @app.after_request
